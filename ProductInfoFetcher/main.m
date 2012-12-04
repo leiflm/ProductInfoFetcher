21169aa7590dddca2301b2b7c3b278e4a8fa17ae
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-12-04 02:35:54 +0100

    Initial Commit

---
+++ ProductInfoFetcher/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProductInfoFetcher
+//
+//  Created by Leif Middelschulte on 04.12.12.
+//  Copyright (c) 2012 Leif Middelschulte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
